bda3ea8c2a60fdefd282ce84d85eceef69947b33
effective-cyborg-8832 <jorgeguilln@gmail.com>
2020-02-28 16:59:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["o","x"] 
+board = ["o","x"]
 def display_board(board)
 puts "   | #{board[1]} |   "
 puts "-----------"
-puts "   |   |   "
+puts "   | #{board[1]} |   "
 puts "-----------"
 puts "   |   |   "
 end
