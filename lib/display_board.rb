f6e68e6f19d73246748d502fb1f73125ba874a9b
effective-cyborg-8832 <jorgeguilln@gmail.com>
2020-02-28 17:17:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = ["X","X","X"," ","X"," "," "," "," "]
 def display_board(board)
-puts " #{board[0]} | #{board[1]} | #{board[3]} "
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts "   | #{board[4]} |   "
 puts "-----------"
